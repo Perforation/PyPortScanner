399663da861523bc4f4369fa4bc2f5b01cf4256a
T. R <Perforation@users.noreply.github.com>
2021-11-28 10:39:52 -0700

    Update portscanner.py

--- portscanner.py
+++ portscanner.py
@@ -1,44 +1,10 @@
-import pyfiglet
-import sys
 import socket
-from datetime import datetime
 
-ascii_banner = pyfiglet.figlet_format("PORT SCANNER")
-print(ascii_banner)
-
-# Defining a target
-if len(sys.argv) == 2:
-
-    # translate hostname to IPv4
-    target = socket.gethostbyname(sys.argv[1])
-else:
-    print("Invalid ammount of Argument")
-
-# Add Banner
-print("-" * 50)
-print("Scanning Target: " + target)
-print("Scanning started at:" + str(datetime.now()))
-print("-" * 50)
-
-try:
-
-    # will scan ports between 1 to 65,535
-    for port in range(1, 65535):
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        socket.setdefaulttimeout(1)
-
-        # returns an error indicator
-        result = s.connect_ex((target, port))
-        if result == 0:
-            print("Port {} is open".format(port))
-        s.close()
-
-except KeyboardInterrupt:
-    print("\n Exitting Program !!!!")
-    sys.exit()
-except socket.gaierror:
-    print("\n Hostname Could Not Be Resolved !!!!")
-    sys.exit()
-except socket.error:
-    print("\ Server not responding !!!!")
-    sys.exit()
+for port in range(1, 9000):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    result = sock.connect_ex(("192.168.88.253", port))
+    if result == 0:
+        print("Port " + str(port) + " is open")
+    else:
+        print("Port " + str(port) +  " is closed")
+    sock.close()
